587809544ed44b622616a1435289e94fd72f6b92
Rein Van Oyen <reinvanoyen@gmail.com>
2017-08-22 14:41:58 +0200

    minor

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const MathUtils = {
-	randFloatBetween: (min, max) => {
-
+	randFloatBetween: ( min, max ) => {
 		return parseFloat( ( Math.random() * ( max - min ) + min ).toFixed( 4 ) );
 	}
 };
